69a8254e1a78f95b97e9ff026c6a213a1e79e12e
alevidals <alejandro.vida.sanchez16@gmail.com>
2023-03-28 21:53:06 +0200

    chore: change search form to use HouseForm library

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -2,6 +2,7 @@ import { cloneElement, ComponentPropsWithoutRef } from "react";
 
 type InputProps = ComponentPropsWithoutRef<"input"> & {
   icon?: JSX.Element;
+  errors?: string[];
 };
 
 export function Input(props: InputProps) {
@@ -10,6 +11,12 @@ export function Input(props: InputProps) {
       <input {...props} />
       <label htmlFor={props.name}>{props.placeholder}</label>
       {props.icon && cloneElement(props.icon, { className: "input__icon" })}
+      {props.errors &&
+        props.errors.map((error) => (
+          <p key={error} className="input__error">
+            {error}
+          </p>
+        ))}
     </div>
   );
 }
--- src/components/molecules/SearchForm.tsx
+++ src/components/molecules/SearchForm.tsx
@@ -1,7 +1,38 @@
 import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
+import { Field, Form } from "houseform";
 import { useEffect, useState } from "react";
 import { Input } from "../atoms/Input";
 import { Option, Select } from "../atoms/Select";
+import { z } from "zod";
+
+type FormType = {
+  questions: number;
+  category: string;
+  difficulty: string;
+};
+
+const difficulties = [
+  {
+    value: "any",
+    text: "Any difficulty",
+  },
+  {
+    value: "easy",
+    text: "Easy",
+  },
+  {
+    value: "medium",
+    text: "Medium",
+  },
+  {
+    value: "hard",
+    text: "Hard",
+  },
+];
+
+const questionsValidator = z.number().refine((val) => val > 0 && val <= 50, {
+  message: "The number of question have to be between 1 and 50",
+});
 
 export function SearchForm() {
   const [categories, setCategories] = useState<Option[]>([]);
@@ -24,35 +55,62 @@ export function SearchForm() {
     getCategories();
   }, []);
 
+  function handleOnSubmit(data: FormType) {
+    console.log(data);
+  }
+
   return (
-    <form className="search__form">
-      <Input
-        placeholder="Number of questions"
-        icon={<QuestionMarkCircleIcon />}
-      />
-      <Select placeholder="Category" options={categories} />
-      <Select
-        placeholder="Difficulty"
-        options={[
-          {
-            value: "any",
-            text: "Any difficulty",
-          },
-          {
-            value: "easy",
-            text: "Easy",
-          },
-          {
-            value: "medium",
-            text: "Medium",
-          },
-          {
-            value: "hard",
-            text: "Hard",
-          },
-        ]}
-      />
-      <button className="btn btn__play">Play!</button>
-    </form>
+    <Form<FormType> onSubmit={handleOnSubmit}>
+      {({ submit }) => (
+        <div className="search__form">
+          <Field
+            name="questions"
+            initialValue={20}
+            onChangeValidate={questionsValidator}
+          >
+            {({ value, setValue, onBlur, errors }) => (
+              <Input
+                type="number"
+                placeholder="Number of questions"
+                icon={<QuestionMarkCircleIcon />}
+                value={value}
+                onChange={(e) => setValue(Number(e.target.value))}
+                onBlur={onBlur}
+                errors={errors}
+              />
+            )}
+          </Field>
+          <Field name="category" initialValue="9">
+            {({ value, setValue, onBlur }) => (
+              <Select
+                placeholder="Category"
+                options={categories}
+                name="category"
+                id="category"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                onBlur={onBlur}
+              />
+            )}
+          </Field>
+          <Field name="difficulty" initialValue="any">
+            {({ value, setValue, onBlur }) => (
+              <Select
+                placeholder="Difficulty"
+                options={difficulties}
+                name="difficulty"
+                id="difficulty"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                onBlur={onBlur}
+              />
+            )}
+          </Field>
+          <button onClick={submit} className="btn btn__play">
+            Play!
+          </button>
+        </div>
+      )}
+    </Form>
   );
 }
