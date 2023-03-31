fd43cbd0b8ea69412e73275e48b393ca902df3f9
alevidals <alejandro.vida.sanchez16@gmail.com>
2023-04-01 01:09:58 +0200

    chore: remove mock values

--- src/atoms/GameAtom.ts
+++ src/atoms/GameAtom.ts
@@ -3,26 +3,5 @@ import { Game } from "@/types/Game";
 
 export const GameAtom = atom<Game>({
   turn: 0,
-  questions: [
-    {
-      correctAnswer: "string",
-      answeredQuestion: "string",
-      correct: true,
-    },
-    {
-      correctAnswer: "string",
-      answeredQuestion: "string",
-      correct: false,
-    },
-    {
-      correctAnswer: "string",
-      answeredQuestion: "string",
-      correct: true,
-    },
-    {
-      correctAnswer: "string",
-      answeredQuestion: "string",
-      correct: false,
-    },
-  ],
+  questions: [],
 });
